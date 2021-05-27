a1c5a7dcadc15eb2aa70b73ca8dcee65eba79fd6
carcinization <80171031+carcinization@users.noreply.github.com>
2021-05-27 11:03:10 -0400

    Update keymap.c

--- keyboards/planck/keymaps/carcinization/keymap.c
+++ keyboards/planck/keymaps/carcinization/keymap.c
@@ -51,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GESC,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC,
     KC_TAB,  KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT ,
-    MT(MOD_LCTL,KC_MINS), KC_LALT, KC_DEL, KC_LGUI, LOWER,   KC_SPC,  KC_LSFT,  RAISE,   KC_LEFT, KC_UP, KC_DOWN,   KC_RGHT
+    MT(MOD_LCTL,KC_MINS), KC_LALT, KC_DEL, KC_LGUI, TT(_LOWER),   KC_SPC,  KC_LSFT,  RAISE,   KC_LEFT, KC_UP, KC_DOWN,   KC_RGHT
 ),
 
 
@@ -68,9 +68,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LOWER] = LAYOUT_ortho_4x12(
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-    KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
-    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______,
-    BACKLIT, BL_STEP, BL_BRTG, BL_TOGG, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
+    KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_4, KC_5,  KC_6, KC_PLUS, KC_MINS,
+    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_1, KC_2, KC_3, KC_ASTR, KC_SLSH,
+    BACKLIT, BL_STEP, BL_BRTG, BL_TOGG, _______, _______, _______, _______, KC_0, KC_DOT, KC_EQL, KC_ENT
 ),
 	
 
