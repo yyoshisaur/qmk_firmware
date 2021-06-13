7cfe21450b13480625e2888bf94d8a0455bdd680
carcinization <80171031+carcinization@users.noreply.github.com>
2021-06-13 18:02:36 -0400

    pimoroni

---
+++ keyboards/reviung41/keymaps/pimoroni/keymap.c
@@ -0,0 +1,154 @@
+/* Copyright 2020 @toastedmangoes
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include QMK_KEYBOARD_H
+#include "pimoroni_trackball.h"
+#include "pointing_device.h"
+
+enum layer_names {
+    _BASE,
+    _LOWER,
+    _RAISE,
+    _ADJUST
+};
+
+enum custom_keycodes {
+  BALL_HUI,//cycles hue
+  BALL_WHT,//cycles white
+  BALL_DEC,//decreased color
+  BALL_SCR,//scrolls
+  BALL_NCL,//left click
+  BALL_RCL,//right click
+  BALL_MCL,//middle click
+};
+
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [_BASE] = LAYOUT_reviung41(
+    KC_TAB,   KC_Q,     KC_W,     KC_F,     KC_P,      KC_G,               KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSPC,
+    KC_LCTL,  KC_A,     KC_S,     KC_D,     KC_F,      KC_G,               KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
+    KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,               KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  RSFT_T(KC_ENT),
+                                            KC_NO,   MO(1),    KC_SPC,   MO(2),    LT(3, KC_RALT)
+  ),
+
+  [_LOWER] = LAYOUT_reviung41(
+    _______,  KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,    KC_PERC,            KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,  KC_DEL,
+    _______,  KC_UNDS,  KC_PLUS,  KC_LCBR,  KC_RCBR,   KC_PIPE,            KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  KC_GRV,   KC_TILD,
+    _______,  KC_ESC,   KC_LGUI,  KC_LALT,  KC_CAPS,   KC_DQUO,            KC_HOME,  KC_END,   KC_PGUP,  KC_PGDN,  KC_PSCR,  RSFT_T(KC_SPC),
+                                            _______,   _______,  KC_ENT,   _______,  _______
+  ),
+
+  [_RAISE] = LAYOUT_reviung41(
+    _______,  KC_1,     KC_2,     KC_3,     KC_4,      KC_5,               KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_DEL,
+    _______,  KC_MINS,  KC_EQL,   KC_LBRC,  KC_RBRC,   KC_BSLS,            KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,
+    _______,  KC_ESC,   KC_RGUI,  KC_RALT,  KC_CAPS,   KC_QUOT,            KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,
+                                            _______,   _______,  KC_BSPC,  _______,  _______
+  ),
+
+  [_ADJUST] = LAYOUT_reviung41(
+    RGB_VAI,   RGB_SAI, RGB_HUI,  RGB_MOD,  XXXXXXX,   RGB_TOG,            XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+    RGB_VAD,   RGB_SAD, RGB_HUD,  RGB_RMOD, XXXXXXX,   XXXXXXX,            XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+    XXXXXXX,   XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,   XXXXXXX,            RESET,    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+                                            _______,   _______,  XXXXXXX,  _______,  _______
+  ),
+};
+
+uint8_t white = 0;
+uint8_t red = 255;
+uint8_t green = 0;
+uint8_t blue = 0;
+
+void ball_increase_hue(void){
+      if(red!=255&&green!=255&&blue!=255){
+        red =255;
+      }
+      if (red==255&&green<255&&blue==0){
+       green += 15;
+      } else if(green==255&&blue==0&&red>0){
+        red-=15;
+      } else if(red==0&&blue<255&&green==255){
+        blue+=15;
+      } else if(blue==255&&green>0&&red==0){
+        green -= 15;
+      } else if(green == 0&&blue==255&&red<255){
+        red +=15;
+      } else if(green ==0&&blue>0&&red==255){
+        blue -=15;
+      }
+      trackball_set_rgbw(red,green,blue,white);
+}
+
+void decrease_color(void){
+  if (green>0){
+    green-=15;
+  }
+  if (red>0){
+    red-=15;
+  }
+  if (blue>0){
+    blue-=15;
+  }
+  trackball_set_rgbw(red,green,blue,white);
+}
+
+void cycle_white(void){
+  if (white<255){
+    white +=15;
+  } else{
+    white=0;
+  }
+  trackball_set_rgbw(red,green,blue,white);
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record){
+  switch (keycode){
+  case  BALL_HUI:
+    if(record->event.pressed){
+      ball_increase_hue();
+    }
+    break;
+
+  case BALL_WHT:
+    if(record-> event.pressed){
+      cycle_white();
+    }
+    break;
+
+  case BALL_DEC:
+   if(record-> event.pressed){
+      decrease_color();
+    }
+    break;
+
+  case BALL_SCR:
+   if(record->event.pressed){
+     trackball_set_scrolling(true);
+   } else{
+     trackball_set_scrolling(false);
+   }
+   break;
+
+  case BALL_NCL:
+     record->event.pressed?register_code(KC_BTN1):unregister_code(KC_BTN1);
+     break;
+  case BALL_RCL:
+      record->event.pressed?register_code(KC_BTN2):unregister_code(KC_BTN2);
+      break;
+  case BALL_MCL:
+      record->event.pressed?register_code(KC_BTN3):unregister_code(KC_BTN3);
+      break;
+  }
+  return true;
+}
---
+++ keyboards/reviung41/keymaps/pimoroni/rules.mk
@@ -0,0 +1,8 @@
+# only uncomment on the side you have your trackball on
+POINTING_DEVICE_ENABLE = yes
+SRC += pimoroni_trackball.c
+QUANTUM_LIB_SRC += i2c_master.c
+OLED_DRIVER_ENABLE = no
+MOUSEKEY_ENABLE = no
+RGBLIGHT_ENABLE = yes
+LTO_ENABLE = yes
