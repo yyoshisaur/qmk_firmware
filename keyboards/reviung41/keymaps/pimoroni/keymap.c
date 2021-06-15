42033782c7c7d9a64c17cf7c0eb697ab50281ce2
carcinization <80171031+carcinization@users.noreply.github.com>
2021-06-14 21:46:45 -0400

    pimoroni pls

--- keyboards/reviung41/keymaps/pimoroni/keymap.c
+++ keyboards/reviung41/keymaps/pimoroni/keymap.c
@@ -38,7 +38,7 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_reviung41(
     KC_TAB,   KC_Q,     KC_W,     KC_F,     KC_P,      KC_G,               KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSPC,
-    KC_LCTL,  KC_A,     KC_S,     KC_D,     KC_F,      KC_G,               KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
+    KC_LCTL,  KC_A,   KC_BTN3, KC_BTN2, KC_BTN1, BALL_SCR,               KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
     KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,               KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  RSFT_T(KC_ENT),
                                             KC_NO,   MO(1),    KC_SPC,   MO(2),    LT(3, KC_RALT)
   ),
--- keyboards/reviung41/keymaps/pimoroni/rules.mk
+++ keyboards/reviung41/keymaps/pimoroni/rules.mk
@@ -1,7 +1,7 @@
 # only uncomment on the side you have your trackball on
 POINTING_DEVICE_ENABLE = yes
-SRC += pimoroni_trackball.c
-QUANTUM_LIB_SRC += i2c_master.c
+SRC = pimoroni_trackball.c
+QUANTUM_LIB_SRC = i2c_master.c
 MOUSEKEY_ENABLE = no
 RGBLIGHT_ENABLE = yes
 LTO_ENABLE = yes
