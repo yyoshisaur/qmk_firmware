16b4aafde06c0e8f9071a522e35dcb8e7aa7f0bf
carcinization <80171031+carcinization@users.noreply.github.com>
2021-07-08 19:56:41 -0400

    Revert "working on debounce"
    This reverts commit f84daff0b087c3e69827ebfb261065b056b22b32.

--- keyboards/reviung41/config.h
+++ keyboards/reviung41/config.h
@@ -35,12 +35,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define DYNAMIC_KEYMAP_LAYER_COUNT 5
 
-#define TAP_CODE_DELAY 35
-#ifdef DEBOUNCE
-#undef DEBOUNCE
-#endif
-
-#define DEBOUNCE 10
+// #define TAP_CODE_DELAY 35
 
 /*
  * Keyboard Matrix Assignments
@@ -99,7 +94,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #endif
 
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
-// #define DEBOUNCE 5
+#define DEBOUNCE 5
 
 /* define if matrix has ghost (lacks anti-ghosting diodes) */
 //#define MATRIX_HAS_GHOST
