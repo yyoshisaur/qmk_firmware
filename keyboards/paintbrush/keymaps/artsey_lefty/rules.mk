eaa89c8d430790a867a7ff2bff53224e0043c1e2
carcinization <80171031+carcinization@users.noreply.github.com>
2021-10-09 17:47:46 -0400

    paintbrush

---
+++ keyboards/paintbrush/keymaps/artsey_lefty/rules.mk
@@ -0,0 +1,8 @@
+COMBO_ENABLE = yes
+OLED_DRIVER_ENABLE = yes
+OLED_ENABLE = yes
+
+# At the moment, defining a DEFAULT_FOLDER doesn't seem to work in a subfolder
+# like this one. If that ever changes, uncomment this:
+# DEFAULT_FOLDER = paintbrush/lefty
+
